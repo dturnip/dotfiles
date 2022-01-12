2a2743aa23472efbcd15c7ff8c769840fbeb30af
dturnip <alexander.xie007@gmail.com>
2022-01-12 14:28:06 +1100

    init

---
+++ sketchybar/plugins/extra/topproc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# TOPPROC=$(ps axo "%cpu,ucomm" | sort -nr | tail +1 | head -n1 | awk '{printf "%.0f%% %s\n", $1, $2}' | sed -e 's/com.apple.//g')
+# CPUP=$(echo $TOPPROC | sed -nr 's/([^\%]+).*/\1/p')
+# 
+# if [ $CPUP -gt 75 ]; then
+#   sketchybar -m --set $NAME label="異 $TOPPROC"
+# else
+#   sketchybar -m --set $NAME label=""
+# fi
+
+TOPPROC=$(ps axo "%cpu,ucomm" | sort -nr | tail +1 | head -n1 | awk '{printf "%.0f%% %s\n", $1, $2}' | sed -e 's/com.apple.//g')
+CPUP=$(echo $TOPPROC | sed -nr 's/([^\%]+).*/\1/p')
+
+if [ $CPUP -gt 75 ]; then
+  sketchybar -m --set $NAME label="$TOPPROC" icon="􀧓" drawing=on
+else
+  sketchybar -m --set $NAME drawing=off
+fi
+
+
